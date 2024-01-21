2f6605db030e9097c4b2aebe8d86257407a7bbe6
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2024-01-21 22:45:02 +0100

    Moved Nav component to layout, removed default styling from next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+import Nav from "@/components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-neutral-900 text-white ${roboto.className}`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
