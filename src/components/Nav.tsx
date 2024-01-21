0a6c37fa12ac4ab8f5de01746e8f8fb4dd6b59a6
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2024-01-21 22:39:23 +0100

    Created Navigation

---
+++ src/components/Nav.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { Nabla } from "next/font/google";
+import { FaGithub } from "react-icons/fa";
+
+const nabla = Nabla({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function Nav() {
+  return (
+    <nav className="flex justify-between items-center px-4 py-3 bg-zinc-900 my-3 mx-3 rounded-xl shadow-zinc-800 shadow">
+      <Link href="/" className={`text-2xl ${nabla.className}`}>
+        Where&apos;s Wally?
+      </Link>
+      <Link
+        href="https://github.com/Kwiasek"
+        target="_blank"
+        rel="noreferrer"
+        className="hidden md:flex items-center gap-1 text-lg text-gray-400 hover:text-current transition-colors"
+      >
+        <FaGithub />
+        Kwiasek
+      </Link>
+      <Link
+        href="/scoreboard"
+        className="text-lg text-gray-400 hover:text-current transition-colors"
+      >
+        Scoreboard
+      </Link>
+    </nav>
+  );
+}
